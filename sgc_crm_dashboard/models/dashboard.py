df1285a978cdacbb6235ac857047aa7a31447c43
contabo-sgc-server <deploy@contabo-sgc.local>
2026-07-23 06:00:04 +0200

    auto: sync 2026-07-23T04:00:03Z

--- sgc_crm_dashboard/models/dashboard.py
+++ sgc_crm_dashboard/models/dashboard.py
@@ -114,7 +114,7 @@ class CRMDashboard(models.AbstractModel):
         cr.execute(f"""
             SELECT COUNT(*)
             FROM mail_tracking_value mtv
-            JOIN ir_model_fields imf ON imf.id = mtv.field_id_id
+            JOIN ir_model_fields imf ON imf.id = mtv.field_id
             WHERE imf.model = 'crm.lead'
               AND imf.name = 'stage_id'
               AND mtv.create_date::date = CURRENT_DATE
